527c7a65854ffdc3d95c1e1f234dd65280e2e44d
MateuszWiorek <mateusz.wiorek@onet.pl>
2020-03-19 16:32:48 +0100

    Changed callback body to set flag canBeShown as true after loading objects.

--- src/aura/WDLC_WishList/WDLC_WishListHelper.js
+++ src/aura/WDLC_WishList/WDLC_WishListHelper.js
@@ -4,14 +4,15 @@
 ({
     doDoInit : function(component, event){
         let getWishListAction = component.get("c.getWishlistedProducts");
-
         getWishListAction.setCallback(this, function(response){
             let state = response.getState();
             if(state === "SUCCESS"){
                 component.set("v.wishlist", response.getReturnValue());
+                component.set("v.canBeShown", true);
             }else{
                 component.set("v.emptyInformation", $A.get("{!$Label.c.WDLC_Error}"));
             }
-        })
+        });
+        $A.enqueueAction(getWishListAction);
     }
 })
