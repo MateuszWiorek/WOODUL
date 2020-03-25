bab790b3449e6a4ee0d874954f4f529c0628507b
MateuszWiorek <mateusz.wiorek@onet.pl>
2020-03-25 16:52:50 +0100

    Changed price and total price of basket

--- src/aura/WDLC_FurnitureOrderPage/WDLC_FurnitureOrderPageHelper.js
+++ src/aura/WDLC_FurnitureOrderPage/WDLC_FurnitureOrderPageHelper.js
@@ -7,11 +7,34 @@
         getListAction.setCallback(this,function(response){
             let state = response.getState();
             if(state === "SUCCESS"){
+                let total = 0;
                 component.set("v.productsToOrder", response.getReturnValue());
+                for(let i of response.getReturnValue()){
+                    total += i.totalCost;
+                    console.log(total);
+                    console.log(i.totalCost);
+                }
+                component.set("v.totalPrice", total);
+                console.log(user);
             }else{
                 //toast will be here
             }
         });
+        let getUserInfoAction = component.get("c.getCurrentUserInfo");
+        getUserInfoAction.setCallback(this, function(response){
+            let state = response.getState();
+            console.log(state);
+            if(state === "SUCCESS"){
+                let userI = response.getReturnValue();
+                console.log(userI);
+                component.set("v.billingCountry", userI.Country);
+                component.set("v.billingCity", userI.City);
+                component.set("v.billingState", userI.State);
+                component.set("v.billingPostalCode", userI.PostalCode);
+                component.set("v.billingStreet", userI.Street);
+            }
+        });
+        $A.enqueueAction(getUserInfoAction);
         $A.enqueueAction(getListAction);
     },
     doMakeOrder : function(component, event){
@@ -28,7 +51,6 @@
             billingCity : city
         };
             let addressJson = JSON.stringify(address);
-            console.log(addressJson);
         let orderAction = component.get("c.makeOrder");
         orderAction.setParams({
             "addressOrder" : addressJson
@@ -37,6 +59,8 @@
             let state = response.getState();
             let toast = component.find("toastComponent")
             if(state === "SUCCESS"){
+                component.set("v.orderSummary", response.getReturnValue());
+                console.log(response.getReturnValue());
                 toast.openInformationToast($A.get("$Label.c.WDLC_OrderSuccess"),"success","Success");
             }else{
                 console.log(response.getError()[0]);
