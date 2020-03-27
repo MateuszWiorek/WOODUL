6630f4c0b3d40bdd605f04ff9f0bfbcb07aea079
MateuszWiorek <mateusz.wiorek@onet.pl>
2020-03-27 07:39:20 +0100

    git

---
+++ src/aura/WDLC_FurnitureProductView/WDLC_FurnitureProductViewController.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Mateusz Wiorek on 23.03.2020.
+ */
+({
+    handleProductId : function(component, event, helper){
+        helper.doHandleProductId(component, event);
+    },
+    doInit : function(component, event, helper){
+        let initAction = component.get("c.getProduct");
+        let productUrl = decodeURIComponent(window.location.pathname.substring(1));
+        let variables = productUrl.split("/");
+        let productToShowId = variables[variables.length-1];
+        initAction.setParams({
+            "productId" : productToShowId
+        });
+        initAction.setCallback(this, function(response){
+            let state = response.getState();
+            if(state === "SUCCESS"){
+                component.set("v.product", response.getReturnValue());
+                component.set("v.productId", productToShowId);
+            }
+        });
+        $A.enqueueAction(initAction);
+    },
+    addToCart : function(component, event, helper){
+        helper.doAddToCart(component,event);
+    },
+    addToWishList : function(component, event, helper){
+        helper.doAddToWishList(component, event);
+    }
+})
