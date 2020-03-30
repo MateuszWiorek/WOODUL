f6cc785397c99e64c937aa76c500927dd0e0b5c0
MateuszWiorek <mateusz.wiorek@onet.pl>
2020-03-30 16:40:02 +0200

    Added modals content to modal component.

--- src/aura/WDLC_OrderHistoryRow/WDLC_OrderHistoryRowController.js
+++ src/aura/WDLC_OrderHistoryRow/WDLC_OrderHistoryRowController.js
@@ -2,20 +2,7 @@
  * Created by Mateusz Wiorek on 25.03.2020.
  */
 ({
-    openModalCase : function(component, event, helper){
-        component.set("v.showDetailsModal", false);
-        component.set("v.showModal", true);
-    },
-    sendDataToCase : function(component, event, helper){
-        helper.doSendDataToCase(component, event);
-    },
-    closeModalCase : function(component, event){
-        component.set("v.showModal", false);
-    },
     openOrderDetails : function(component){
-        component.set("v.showDetailsModal", true);
-    },
-    closeOrderDetails : function(component){
-        component.set("v.showDetailsModal", false);
+        component.set("v.showModal", true);
     },
 })
--- src/aura/WDLC_OrderHistoryRow/WDLC_OrderHistoryRowHelper.js
+++ src/aura/WDLC_OrderHistoryRow/WDLC_OrderHistoryRowHelper.js
@@ -2,36 +2,4 @@
  * Created by Mateusz Wiorek on 25.03.2020.
  */
 ({
-    doSendDataToCase : function(component, event){
-        let createCaseAction = component.get("c.addComplaint");
-        let prodName = component.get("v.product");
-        let sub = component.get("v.subject");
-        let desc = component.get("v.description");
-        console.log(prodName);
-        createCaseAction.setParams({
-            "productName" : prodName,
-            "subject" : sub,
-            "description" : desc
-        });
-
-        createCaseAction.setCallback(this, function(response){
-            let state = response.getState();
-            if(state === "SUCCESS"){
-                console.log(response);
-                let toast = component.find("toastComponent");
-                if(response.getReturnValue() == true){
-                    toast.openInformationToast("Your complaint has been sent to our support", "success", "success");
-                }else{
-                    toast.openInformationToast("We encountered an error while we was creating your complaint. Please try later."
-                            , "error", "error");
-                }
-                    component.set("v.showModal", false);
-            }else{
-                let toast = component.find("toastComponent");
-                toast.openInformationToast(response.getError()[0], "error", "error");
-            }
-        });
-
-        $A.enqueueAction(createCaseAction);
-    }
 })
