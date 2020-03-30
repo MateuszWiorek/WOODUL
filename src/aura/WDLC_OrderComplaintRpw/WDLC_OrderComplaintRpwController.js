7a0ad1f4a28f00cd8e123c99d62914f2055cff73
MateuszWiorek <mateusz.wiorek@onet.pl>
2020-03-30 17:58:13 +0200

    Added modal to external component.

--- src/aura/WDLC_OrderComplaintRpw/WDLC_OrderComplaintRpwController.js
+++ src/aura/WDLC_OrderComplaintRpw/WDLC_OrderComplaintRpwController.js
@@ -18,38 +18,6 @@
     },
     openModal : function(component){
         component.set("v.showModal", true);
-        let pes = component.find('peService');
-        pes.subscribe("WDLC_NewCaseComment__e",function(){
-            console.log('subscribed');
-        })
-    },
-    postComment : function(component, event, helper){
-        let postAction = component.get("c.postCommentToCase");
-        postAction.setParams({
-            "caseId" : component.get("v.complaint").Id,
-            "bodyComment" : component.get("v.caseCommentMessage")
-        });
-        postAction.setCallback(this,function(response){
-            if(response.getState() === "SUCCESS"){
-                let caseCommentsAction = component.get("c.getCaseComments");
-                caseCommentsAction.setParams({
-                    "caseId" : component.get("v.complaint").Id
-                });
-                caseCommentsAction.setCallback(this, function(response){
-                    let state = response.getState();
-                    if(state === "SUCCESS"){
-                        component.set("v.caseComments", response.getReturnValue());
-                        component.set("v.caseCommentMessage","");
-                    }else{
-                    }
-                });
-                $A.enqueueAction(caseCommentsAction);
-            }
-        });
-        $A.enqueueAction(postAction);
-    },
-    closeModal : function(component, event){
-        component.set("v.showModal",false);
     },
 
 
