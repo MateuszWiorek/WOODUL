a5d4d0977f81724e86a8b41d4fadb41800953612
MateuszWiorek <mateusz.wiorek@onet.pl>
2020-04-14 10:37:25 +0200

    Added paddings, dark backgroundcolor for order button, and add unit price near to total price, and restrict width of quantity column

---
+++ src/aura/WDLC_FurnitureOrderPage/WDLC_FurnitureOrderPageController.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Mateusz Wiorek on 24.03.2020.
+ */
+({
+    doInit : function(component, event, helper){
+        helper.doOnInit(component, event);
+    },
+    makeOrderr : function(component, event, helper){
+        helper.doMakeOrder(component, event);
+        helper.openModal(component);
+        helper.doRefreshTable(component,event);
+    },
+    incrementCounter : function(component,event,helper){
+        helper.doIncrementCounter(component,event);
+    },
+    decrementCounter : function(component, event, helper){
+        helper.doDecrementCounter(component,event);
+    },
+    refreshTable : function(component, event, helper){
+        helper.doRefreshTable(component, event);
+    }
+})
---
+++ src/aura/WDLC_FurnitureOrderPage/WDLC_FurnitureOrderPageHelper.js
@@ -0,0 +1,100 @@
+/**
+ * Created by Mateusz Wiorek on 24.03.2020.
+ */
+({
+    doOnInit : function(component, event){
+        let getListAction = component.get("c.getProductsToOrder");
+               getListAction.setCallback(this,function(response){
+                   let state = response.getState();
+                   if(state === "SUCCESS"){
+                       let total = 0;
+                       let products = response.getReturnValue();
+                       for(let i = 0; i<products.length; i++){
+                           total +=products[i].totalCost;
+                       }
+                       component.set("v.totalPrice", total);
+                       component.set("v.productsToOrder", products);
+                   }else{
+                   }
+               });
+               let getUserInfoAction = component.get("c.getCurrentUserInfo");
+               getUserInfoAction.setCallback(this, function(response){
+                   let state = response.getState();
+                   console.log(state);
+                   if(state === "SUCCESS"){
+                       let userI = response.getReturnValue();
+                       console.log(userI);
+                       component.set("v.billingCountry", userI.Country);
+                       component.set("v.billingCity", userI.City);
+                       component.set("v.billingState", userI.State);
+                       component.set("v.billingPostalCode", userI.PostalCode);
+                       component.set("v.billingStreet", userI.Street);
+                   }
+               });
+               $A.enqueueAction(getUserInfoAction);
+               $A.enqueueAction(getListAction);
+    },
+    doMakeOrder : function(component, event){
+        let street = component.get("v.billingStreet");
+        let postalCode = component.get("v.billingPostalCode");
+        let state = component.get("v.billingState");
+        let country = component.get("v.billingCountry");
+        let city = component.get("v.billingCity");
+        let address = {
+            billingStreet : street,
+            billingPostalCode : postalCode,
+            billingState : state,
+            billingCountry : country,
+            billingCity : city
+        };
+            let addressJson = JSON.stringify(address);
+        let orderAction = component.get("c.makeOrder");
+        orderAction.setParams({
+            "addressOrder" : addressJson
+        });
+        orderAction.setCallback(this, function(response){
+            let state = response.getState();
+            let toast = component.find("toastComponent")
+            if(state === "SUCCESS"){
+                component.set("v.orderSummary", response.getReturnValue());
+                component.set("v.showModal",true);
+                console.log(response.getReturnValue());
+                toast.openInformationToast($A.get("$Label.c.WDLC_OrderSuccess"),"success","Success");
+            }else{
+                console.log(response.getError()[0]);
+                toast.openInformationToast(response.getError()[0].message, $A.get("$Label.c.WDL_Error"),
+                 $A.get("$Label.c.WDL_Error"));
+            }
+        });
+        $A.enqueueAction(orderAction);
+    },
+    doIncrementCounter : function(component, event){
+        let indexOfItem = event.currentTarget.dataset.record;
+        console.log('1'+indexOfItem);
+        let items = component.get("v.productsToOrder")[indexOfItem];
+        console.log(items);
+        let incrementAction = component.get("c.increaseProductCounter");
+        incrementAction.setParams({
+            "productId" : idOfProduct
+        });
+    },
+    doRefreshTable : function(component, event){
+        console.log('hi');
+        let getItemsAction = component.get("c.getProductsToOrder");
+        getItemsAction.setCallback(this,function(response){
+            let state = response.getState();
+            if(state==="SUCCESS"){
+                 let total = 0;
+                 component.set("v.productsToOrder", response.getReturnValue());
+                 for(let i of response.getReturnValue()){
+                    total += i.totalCost;
+                 }
+                 component.set("v.totalPrice", total);
+            }
+        });
+        $A.enqueueAction(getItemsAction);
+    },
+    openModal : function(component){
+        component.set("v.showModal", true);
+    },
+})
