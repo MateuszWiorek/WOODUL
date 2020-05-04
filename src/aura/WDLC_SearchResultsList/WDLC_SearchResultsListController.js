8a275c25baa29809784670b7d981704f68fae99a
MateuszWiorek <mateusz.wiorek@onet.pl>
2020-05-04 07:42:33 +0200

    Added pagination for search results list

--- src/aura/WDLC_SearchResultsList/WDLC_SearchResultsListController.js
+++ src/aura/WDLC_SearchResultsList/WDLC_SearchResultsListController.js
@@ -9,5 +9,40 @@
          meta.setAttribute("content", "date=no;img-src 'self' data:");
          document.getElementsByTagName('head')[0].appendChild(meta);
         helper.doOnInit(component,event);
+     },
+     goNextPage: function(component,event,helper){
+         component.set("v.productsToShowStart", component.get("v.productsToShowStart") + component.get("v.productsToShow"));
+         component.set("v.productsToShowEnd", component.get("v.productsToShowEnd") + component.get("v.productsToShow"));
+     },
+     goPreviousPage: function(component, event, helper){
+         component.set("v.productsToShowStart", component.get("v.productsToShowStart") - component.get("v.productsToShow"));
+         component.set("v.productsToShowEnd", component.get("v.productsToShowEnd") - component.get("v.productsToShow"));
+     },
+     changeShowItemsValue:function(component,event, helper){
+         component.set("v.productsToShow", component.find("selectProductsCounter").get("v.value"));
+         component.set("v.productsToShowEnd", component.get("v.productsToShowStart") + component.get("v.productsToShow"));
+     },
+     sortByPrice:function(component, event, helper){
+         component.set("v.hideSpinner", false);
+         let res = component.get("v.results");
+         let sortType = component.find("selectSortType").get("v.value");
+         if(sortType == 'Price asc'){
+            res.sort((a,b) => (parseFloat(a.productPriceAfterDiscount) > parseFloat(b.productPriceAfterDiscount)) ? 1 : -1);
+         }else if(sortType == 'Price desc'){
+            res.sort((a,b) => (parseFloat(a.productPriceAfterDiscount) < parseFloat(b.productPriceAfterDiscount)) ? 1 : -1);
+         }else if(sortType == 'Name asc'){
+            res.sort((a,b) => (a.productName > b.productName) ? 1 : -1);
+         }else if(sortType == 'Name desc'){
+            res.sort((a,b) => (a.productName < b.productName) ? 1 : -1);
+         }
+         console.log(res);
+         component.set("v.results", res);
+         component.set("v.hideSpinner", true);
+     },
+     waiting: function(component, event, helper) {
+       component.set("v.hideSpinner", true);
+     },
+     doneWaiting: function(component, event, helper) {
+       component.set("v.hideSpinner", false);
      }
 })
